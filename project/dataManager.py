f06f5a746faf5a5320a80333d399accfb4e4064b
Thibault Duhamel <thibault.duhamel@ensiie.fr>
2019-02-18 16:59:17 -0500

    DataManager rename

--- project/dataManager.py
+++ project/dataManager.py
@@ -14,7 +14,7 @@ class DataManager():
 	Reads the csv files located at path_train and path_test, and stores them in the class variables defined above
 	"""
 	def load_CSV(self, path_train, path_test):
-		print("------------------------")
+		print("")
 		print("Loading training data...")
 		#First part : training data
 		with open(path_train) as csv_file_train:
@@ -28,7 +28,7 @@ class DataManager():
 			labels_string = []
 			for row in csv_file_train_reader:
 				labels_string.append(row[1])
-				features.append(row[2:])
+				features.append([float(feature) for feature in row[2:]])
 			self.x_train = np.array(features)
 			
 			#Establish a link between leaf names and unique assigned ids
@@ -50,10 +50,6 @@ class DataManager():
 			#list of the 192 features
 			features = []
 			for row in csv_file_test_reader:
-				features.append(row[1:])
+				features.append([float(feature) for feature in row[1:]])
 			self.x_test = np.array(features)
 		print("-> " + str(self.x_test.shape[0]) + " testing examples loaded")
-		print("------------------------")
-
-dm = DataManager()
-dm.load_CSV("leaf-classification/train.csv", "leaf-classification/test.csv")
