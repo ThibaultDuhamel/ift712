a4fddee307b96bcd6d757867624927ea89f9e362
Thibault Duhamel <thibault.duhamel@usherbrooke.ca>
2019-03-14 16:59:44 -0400

    entrainement, prediction, erreur, affichage erreur

--- TP3/map_noyau.py
+++ TP3/map_noyau.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 #####
-# Vos Noms (Vos Matricules) .~= À MODIFIER =~.
+# DUHAMEL Thibault 18026048
+# SHI Heng 18171434
 ###
 
 import numpy as np
@@ -9,97 +10,120 @@ import matplotlib.pyplot as plt
 
 
 class MAPnoyau:
-    def __init__(self, lamb=0.2, sigma_square=1.06, b=1.0, c=0.1, d=1.0, M=2, noyau='rbf'):
-        """
-        Classe effectuant de la segmentation de données 2D 2 classes à l'aide de la méthode à noyau.
+	def __init__(self, lamb=0.2, sigma_square=1.06, b=1.0, c=0.1, d=1.0, M=2, noyau='rbf'):
+		"""
+		Classe effectuant de la segmentation de données 2D 2 classes à l'aide de la méthode à noyau.
 
-        lamb: coefficiant de régularisation L2
-        sigma_square: paramètre du noyau rbf
-        b, d: paramètres du noyau sigmoidal
-        M,c: paramètres du noyau polynomial
-        noyau: rbf, lineaire, olynomial ou sigmoidal
-        """
-        self.lamb = lamb
-        self.a = None
-        self.sigma_square = sigma_square
-        self.M = M
-        self.c = c
-        self.b = b
-        self.d = d
-        self.noyau = noyau
-        self.x_train = None
+		lamb: coefficiant de régularisation L2
+		sigma_square: paramètre du noyau rbf
+		b, d: paramètres du noyau sigmoidal
+		M,c: paramètres du noyau polynomial
+		noyau: rbf, lineaire, polynomial ou sigmoidal
+		"""
+		self.lamb = lamb
+		self.a = None
+		self.sigma_square = sigma_square
+		self.M = M
+		self.c = c
+		self.b = b
+		self.d = d
+		self.noyau = noyau
+		self.x_train = None
 
-        
 
-    def entrainement(self, x_train, t_train):
-        """
-        Entraîne une méthode d'apprentissage à noyau de type Maximum a
-        posteriori (MAP) avec un terme d'attache aux données de type
-        "moindre carrés" et un terme de lissage quadratique (voir
-        Eq.(1.67) et Eq.(6.2) du livre de Bishop).  La variable x_train
-        contient les entrées (un tableau 2D Numpy, où la n-ième rangée
-        correspond à l'entrée x_n) et des cibles t_train (un tableau 1D Numpy
-        où le n-ième élément correspond à la cible t_n).
 
-        L'entraînement doit utiliser un noyau de type RBF, lineaire, sigmoidal,
-        ou polynomial (spécifié par ''self.noyau'') et dont les parametres
-        sont contenus dans les variables self.sigma_square, self.c, self.b, self.d
-        et self.M et un poids de régularisation spécifié par ``self.lamb``.
+	def entrainement(self, x_train, t_train):
+		"""
+		Entraîne une méthode d'apprentissage à noyau de type Maximum a
+		posteriori (MAP) avec un terme d'attache aux données de type
+		"moindre carrés" et un terme de lissage quadratique (voir
+		Eq.(1.67) et Eq.(6.2) du livre de Bishop).  La variable x_train
+		contient les entrées (un tableau 2D Numpy, où la n-ième rangée
+		correspond à l'entrée x_n) et des cibles t_train (un tableau 1D Numpy
+		où le n-ième élément correspond à la cible t_n).
 
-        Cette méthode doit assigner le champs ``self.a`` tel que spécifié à
-        l'equation 6.8 du livre de Bishop et garder en mémoire les données
-        d'apprentissage dans ``self.x_train``
-        """
-        #AJOUTER CODE ICI
-        
-    def prediction(self, x):
-        """
-        Retourne la prédiction pour une entrée representée par un tableau
-        1D Numpy ``x``.
+		L'entraînement doit utiliser un noyau de type RBF, lineaire, sigmoidal,
+		ou polynomial (spécifié par ''self.noyau'') et dont les parametres
+		sont contenus dans les variables self.sigma_square, self.c, self.b, self.d
+		et self.M et un poids de régularisation spécifié par ``self.lamb``.
 
-        Cette méthode suppose que la méthode ``entrainement()`` a préalablement
-        été appelée. Elle doit utiliser le champs ``self.a`` afin de calculer
-        la prédiction y(x) (équation 6.9).
+		Cette méthode doit assigner le champs ``self.a`` tel que spécifié à
+		l'equation 6.8 du livre de Bishop et garder en mémoire les données
+		d'apprentissage dans ``self.x_train``
+		"""
+		self.x_train = x_train
 
-        NOTE : Puisque nous utilisons cette classe pour faire de la
-        classification binaire, la prediction est +1 lorsque y(x)>0.5 et 0
-        sinon
-        """
-        #AJOUTER CODE ICI
-        return 0
+		N = x_train.shape[0]
 
-    def erreur(self, t, prediction):
-        """
-        Retourne la différence au carré entre
-        la cible ``t`` et la prédiction ``prediction``.
-        """
-        # AJOUTER CODE ICI
-        return 0.
+		K = np.zeros((N,N))
+		for i in range(N):
+			for j in range(i+1):
+				k_xi_xj = 0
+				if self.noyau == "rbf":
+					k_xi_xj = np.exp(-np.linalg.norm(x_train[i]-x_train[j])**2/2/self.sigma_square)
+				elif self.noyau == "polynomial":
+					k_xi_xj = (np.dot(x_train[i],x_train[j])+self.c)**self.M
+				elif self.noyau == "sigmoidal":
+					k_xi_xj = np.tanh(self.b*np.dot(x_train[i],x_train[j]) + self.d)
+				else:
+					k_xi_xj = np.dot(x_train[i],x_train[j])
+				#K is symetric
+				K[i,j] = k_xi_xj
+				K[j,i] = k_xi_xj
 
-    def validation_croisee(self, x_tab, t_tab):
-        """
-        Cette fonction trouve les meilleurs hyperparametres ``self.sigma_square``,
-        ``self.c`` et ``self.M`` (tout dépendant du noyau selectionné) et
-        ``self.lamb`` avec une validation croisée de type "k-fold" où k=1 avec les
-        données contenues dans x_tab et t_tab.  Une fois les meilleurs hyperparamètres
-        trouvés, le modèle est entraîné une dernière fois.
+		#Equation 6.8
+		self.a = np.dot(np.linalg.inv(K + self.lamb*np.identity(x_train.shape[0])),t_train)
 
-        SUGGESTION: Les valeurs de ``self.sigma_square`` et ``self.lamb`` à explorer vont
-        de 0.000000001 à 2, les valeurs de ``self.c`` de 0 à 5, les valeurs
-        de ''self.b'' et ''self.d'' de 0.00001 à 0.01 et ``self.M`` de 2 à 6
-        """
-        # AJOUTER CODE ICI
+	def prediction(self, x):
+		"""
+		Retourne la prédiction pour une entrée representée par un tableau
+		1D Numpy ``x``.
 
-    def affichage(self, x_tab, t_tab):
+		Cette méthode suppose que la méthode ``entrainement()`` a préalablement
+		été appelée. Elle doit utiliser le champs ``self.a`` afin de calculer
+		la prédiction y(x) (équation 6.9).
 
-        # Affichage
-        ix = np.arange(x_tab[:, 0].min(), x_tab[:, 0].max(), 0.1)
-        iy = np.arange(x_tab[:, 1].min(), x_tab[:, 1].max(), 0.1)
-        iX, iY = np.meshgrid(ix, iy)
-        x_vis = np.hstack([iX.reshape((-1, 1)), iY.reshape((-1, 1))])
-        contour_out = np.array([self.prediction(x) for x in x_vis])
-        contour_out = contour_out.reshape(iX.shape)
+		NOTE : Puisque nous utilisons cette classe pour faire de la
+		classification binaire, la prediction est +1 lorsque y(x)>0.5 et 0
+		sinon
+		"""
+		#Equation 6.9
+		y = np.dot(self.a,np.dot(self.x_train,x))
+		if y>0.5:
+			return 1
+		return 0
 
-        plt.contourf(iX, iY, contour_out > 0.5)
-        plt.scatter(x_tab[:, 0], x_tab[:, 1], s=(t_tab + 0.5) * 100, c=t_tab, edgecolors='y')
-        plt.show()
+	def erreur(self, t, prediction):
+		"""
+		Retourne la différence au carré entre
+		la cible ``t`` et la prédiction ``prediction``.
+		"""
+		return (t-prediction)**2
+
+	def validation_croisee(self, x_tab, t_tab):
+		"""
+		Cette fonction trouve les meilleurs hyperparametres ``self.sigma_square``,
+		``self.c`` et ``self.M`` (tout dépendant du noyau selectionné) et
+		``self.lamb`` avec une validation croisée de type "k-fold" où k=1 avec les
+		données contenues dans x_tab et t_tab.  Une fois les meilleurs hyperparamètres
+		trouvés, le modèle est entraîné une dernière fois.
+
+		SUGGESTION: Les valeurs de ``self.sigma_square`` et ``self.lamb`` à explorer vont
+		de 0.000000001 à 2, les valeurs de ``self.c`` de 0 à 5, les valeurs
+		de ''self.b'' et ''self.d'' de 0.00001 à 0.01 et ``self.M`` de 2 à 6
+		"""
+		# AJOUTER CODE ICI
+
+	def affichage(self, x_tab, t_tab):
+
+		# Affichage
+		ix = np.arange(x_tab[:, 0].min(), x_tab[:, 0].max(), 0.1)
+		iy = np.arange(x_tab[:, 1].min(), x_tab[:, 1].max(), 0.1)
+		iX, iY = np.meshgrid(ix, iy)
+		x_vis = np.hstack([iX.reshape((-1, 1)), iY.reshape((-1, 1))])
+		contour_out = np.array([self.prediction(x) for x in x_vis])
+		contour_out = contour_out.reshape(iX.shape)
+
+		plt.contourf(iX, iY, contour_out > 0.5)
+		plt.scatter(x_tab[:, 0], x_tab[:, 1], s=(t_tab + 0.5) * 100, c=t_tab, edgecolors='y')
+		plt.show()
