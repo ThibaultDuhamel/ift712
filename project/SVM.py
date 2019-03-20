f5920d795bc3b0b10a911121d1f136a734cc598b
Thibault Duhamel <thibault.duhamel@usherbrooke.ca>
2019-03-20 11:07:14 -0400

    SVM initialization + train

--- project/SVM.py
+++ project/SVM.py
@@ -1,26 +1,43 @@
 import numpy as np
 from sklearn import svm
+from Classifier import *
+from DataManager import *
 
 #Super class for each classifier method
 class SVM(Classifier):
 
 	#Initialisation of the required parameters
 	def __init__(self):
-		pass
+		#SVM penalty parameter, to control the softness of the separation
+		self.C = 1.0
+		#SVM kernel, which can be linear, rbf, sigmoidal, poly
+		self.kernel = "linear"
+
+		#If kernel == rbf or poly or sigmoid, control the kernel coefficient
+		self.gamma = 0.1
+
+		#If kernel == poly, control the polynom degree
+		self.degree = 1
+
+		#Build the SVM model with sklearn
+		self.model = None
 
 	"""
 	Train the model with the x_train features array and the y_train one-hot labels
 	Return : nothing
 	"""
 	def train(self, x_train, y_train):
-		pass
+		#Construct the model with all the hyperparameters, even though some may not be used
+		self.model = svm.SVC(kernel=self.kernel, C=self.C, gamma=self.gamma, degree=self.degree)
+		#Train the model
+		self.model.fit(x_train,y_train)
 
 	"""
 	Test the model with the x_test features array
 	Return : an array of predicted labels, as probabilities for each class
 	"""
 	def test(self, x_test):
-		pass
+		return self.model.predict(x_test)
 
 	"""
 	Train/test the model with different hyper parameters values, and select the best ones
@@ -29,14 +46,9 @@ class SVM(Classifier):
 	def cross_validation(self, x_train, y_train):
 		pass
 
-	"""
-	Compute the percentage of correct predictions over the total number of predictions (100*correct/total).
-	The two inputs must be arrays of probabilities
-	Return : the percentage of correct predictions, a float from 0 to 100
-	"""
-	def accuracy(self, predicted_labels, true_labels):
-		accuracy = 0.0
-		for predicted,true in zip(predicted_labels,true_labels):
-			if np.argmax(predicted) == np.argmax(true):
-				accuracy+=1
-		return 100*accuracy/predicted_labels.shape[0]
+dm = DataManager()
+dm.load_CSV("leaf-classification/train.csv", "leaf-classification/test.csv")
+s = SVM()
+s.train(dm.x_train, dm.y_train_strings)
+print("")
+print("Best C =",s.C)
