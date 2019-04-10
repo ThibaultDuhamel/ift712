e81a0984e43428f8ef672811a7f459ce96b7ca73
SharonSHH <shiheng0828@gmail.com>
2019-04-10 09:58:08 -0400

    Add integer encoding to y

--- project/DataManager.py
+++ project/DataManager.py
@@ -2,62 +2,73 @@ import csv
 import numpy as np
 import pandas as pd
 
+
 class DataManager():
 
-	def __init__(self):
-		#Lists of leaf features examples and labels for training
-		self.x_train = np.array([])
-		self.y_train = np.array([])
-		#Lists of leaf features examples without labels for testing
-		self.x_test = np.array([])
+    def __init__(self):
+        # Lists of leaf features examples and labels for training
+        self.x_train = np.array([])
+        self.y_train = np.array([])
+        self.y_train_integer = np.array([])
+        # Lists of leaf features examples without labels for testing
+        self.x_test = np.array([])
 
-	"""
-	Reads the csv files located at path_train and path_test, and stores them in the class variables defined above
-	"""
-	def load_CSV(self, path_train, path_test):
-		print("")
-		print("Loading training data...")
-		#First part : training data
-		with open(path_train) as csv_file_train:
-			csv_file_train_reader = csv.reader(csv_file_train)
-			#Skip header
-			next(csv_file_train_reader)
+    def load_CSV(self, path_train, path_test):
+        """
+        Reads the csv files located at path_train and path_test,
+        and stores them in the class variables defined above
+        """
+        print("")
+        print("Loading training data...")
+        # First part : training data
+        with open(path_train) as csv_file_train:
+            csv_file_train_reader = csv.reader(csv_file_train)
+            # Skip header
+            next(csv_file_train_reader)
 
-			#list of the 192 features
-			features = []
-			#list of the leaf labels (string labels) corresponding to the features
-			labels_string = []
-			for row in csv_file_train_reader:
-				labels_string.append(row[1])
-				features.append([float(feature) for feature in row[2:]])
-			self.x_train = np.array(features)
+            # list of the 192 features
+            features = []
+            """
+            list of the leaf labels (string labels) corresponding to the features
+            """
+            labels_string = []
+            for row in csv_file_train_reader:
+                labels_string.append(row[1])
+                features.append([float(feature) for feature in row[2:]])
+            self.x_train = np.array(features)
 
-			#Establish a link between leaf names and unique assigned ids
-			unique_labels = np.unique(labels_string)
-			#Leaf labels (but this time converted to one-hot vectors) corresponding to the features
-			y_value = list(range(0,len(unique_labels)))
-			y_dict = dict(zip(unique_labels, y_value))
-			temp = pd.Series(labels_string)
-			self.y_train = temp.map(y_dict)
-			'''
-			self.y_train = np.zeros((len(features),
-			unique_labels.shape[0]))
-			for l in range(len(labels_string)):
-				self.y_train[l][np.where(unique_labels==labels_string[l])[0]] = 1
-			'''
-			
-		print("-> " + str(self.x_train.shape[0]) + " training examples loaded")
+            # Establish a link between leaf names and unique assigned ids
+            unique_labels = np.unique(labels_string)
+            """
+            Leaf labels (but this time converted to one-hot vectors) corresponding to the features
+            """
+            self.y_train = np.zeros((len(features),
+                                     unique_labels.shape[0]))
+            for l in range(len(labels_string)):
+                self.y_train[l][np.where(
+                    unique_labels == labels_string[l])[0]] = 1
 
-		print("Loading testing data...")
-		#Second part : testing data
-		with open(path_test) as csv_file_test:
-			csv_file_test_reader = csv.reader(csv_file_test)
-			#Skip header
-			next(csv_file_test_reader)
+            """
+            Leaf labels - Integer Encoding (but this time converted to
+                integer vector) corresponding to the features
+            """
+            y_value = list(range(0, len(unique_labels)))
+            y_dict = dict(zip(unique_labels, y_value))
+            temp = pd.Series(labels_string)
+            self.y_train_integer = temp.map(y_dict)
 
-			#list of the 192 features
-			features = []
-			for row in csv_file_test_reader:
-				features.append([float(feature) for feature in row[1:]])
-			self.x_test = np.array(features)
-		print("-> " + str(self.x_test.shape[0]) + " testing examples loaded")
+        print("-> " + str(self.x_train.shape[0]) + " training examples loaded")
+
+        print("Loading testing data...")
+        # Second part : testing data
+        with open(path_test) as csv_file_test:
+            csv_file_test_reader = csv.reader(csv_file_test)
+            # Skip header
+            next(csv_file_test_reader)
+
+            # list of the 192 features
+            features = []
+            for row in csv_file_test_reader:
+                features.append([float(feature) for feature in row[1:]])
+            self.x_test = np.array(features)
+        print("-> " + str(self.x_test.shape[0]) + " testing examples loaded")
